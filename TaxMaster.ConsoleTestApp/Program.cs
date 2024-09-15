a70ecb06419606e7f4399e6857602ce8a1d1e01a
Yovel Mordechai Ben Harosh <ybenharosh@microsoft.com>
2024-09-15 12:55:34 +0300

    Add Exchange Currency Client

---
+++ TaxMaster.ConsoleTestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using TaxMaster.TaxMaster.Infra;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var exchangeCurrencyClient = new ExchangeCurrencyClient();
+
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                double exchangeRate = await exchangeCurrencyClient.GetExchangeRateAsync(DateTime.Now - TimeSpan.FromDays(-i));
+                Console.WriteLine($"Exchange rate: {exchangeRate}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to get exchange rate: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ TaxMaster.Infra/Clients/ExchangeCurrencyClient.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Xml.Linq;
+
+namespace TaxMaster.TaxMaster.Infra;
+
+public class ExchangeCurrencyClient
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    public async Task<double> GetExchangeRateAsync(DateTime date)
+    {
+        int cntr = 0;
+        HttpResponseMessage response;
+        do
+        {
+            date = date.AddDays(-cntr);
+            string formattedDate = date.ToString("yyyy-MM-dd");
+            string url = $"https://edge.boi.org.il/FusionEdgeServer/sdmx/v2/data/dataflow/BOI.STATISTICS/EXR/1.0/RER_USD_ILS?c%5BDATA_TYPE%5D=OF00&startperiod={formattedDate}&endperiod={formattedDate}&format=csv";
+
+            response = await client.GetAsync(url);
+        }
+        while (cntr++ < 10 && response.StatusCode == HttpStatusCode.NotFound);
+
+        response.EnsureSuccessStatusCode();
+
+        string csvContent = await response.Content.ReadAsStringAsync();
+        using (var reader = new StringReader(csvContent))
+        {
+            string? headerLine = reader.ReadLine();
+            string? dataLine = reader.ReadLine();
+
+            if (headerLine == null || dataLine == null)
+            {
+                throw new Exception("Invalid CSV format.");
+            }
+
+            string[] headers = headerLine.Split(',');
+            string[] data = dataLine.Split(',');
+
+            int obsValueIndex = Array.IndexOf(headers, "OBS_VALUE");
+            if (obsValueIndex == -1)
+            {
+                throw new Exception("OBS_VALUE column not found.");
+            }
+
+            if (double.TryParse(data[obsValueIndex], NumberStyles.Any, CultureInfo.InvariantCulture, out double exchangeRate))
+            {
+                return exchangeRate;
+            }
+
+            throw new Exception("Invalid OBS_VALUE format.");
+        }
+    }
+}
