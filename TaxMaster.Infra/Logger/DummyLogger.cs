acb863b2f2d91961dc859bb503945b92e828de54
Shimon Arzuan <sharzuan@microsoft.com>
2024-09-15 16:03:02 +0300

    add logger

---
+++ TaxMaster.Infra/Logger/DummyLogger.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TaxMaster.Infra
+{
+    public class DummyLogger : ILogger
+    {
+        public static ILogger CreateLogger()
+        {
+            return new DummyLogger();
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return false;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            return;
+        }
+    }
+}
