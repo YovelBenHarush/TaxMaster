5e22a78815a918aa87ebb177bb35f0172eb74cb6
Shimon Arzuan <58290359+shimonar1171@users.noreply.github.com>
2024-09-16 11:21:24 +0300

    Add pdf loader (#16)

--- TaxMaster.UI/ViewModels/EsppDividendsViewModel.cs
+++ TaxMaster.UI/ViewModels/EsppDividendsViewModel.cs
@@ -2,6 +2,28 @@
 {
     public class EsppDividendsViewModel : BaseViewModel
     {
+        private string _pdfUrl;
+
+        public string PdfUrl
+        {
+            get => _pdfUrl;
+            set
+            {
+                if (_pdfUrl != value)
+                {
+                    _pdfUrl = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public EsppDividendsViewModel()
+        {
+            // Local PDF file (place this file in your app's Resources folder)
+            PdfUrl = GuidesConfigurations.EsppFidelityPdfUrl;
+        }
+
+
         public override void OnNext()
         {
             throw new NotImplementedException();
