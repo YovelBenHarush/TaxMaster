9d28a8d3a9d2c55b6edbbd8fd4f6a3e1fc2096ce
Shimon Arzuan <58290359+shimonar1171@users.noreply.github.com>
2024-09-16 21:27:57 +0300

    Sharzuan/add 92 expnations (#28)
    * Update annual report
    
    * Add tax accuont 92 expnations

--- TaxMaster.UI/ViewModels/TaxAccountConfirmationViewModel.cs
+++ TaxMaster.UI/ViewModels/TaxAccountConfirmationViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace TaxMaster
+﻿using System.Windows.Input;
+
+namespace TaxMaster
 {
     public class TaxAccountConfirmationViewModel : BaseViewModel
     {
+        public ICommand OpenLinkCommand { get; }
+
+        public TaxAccountConfirmationViewModel()
+        {
+            OpenLinkCommand = new Command<string>(OpenLink);
+        }
+
+        private async void OpenLink(string url)
+        {
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                await Launcher.OpenAsync(url);
+            }
+        }
+
         public async override void OnNext()
         {
             await Shell.Current.GoToAsync(nameof(PersonalDetailsView));
