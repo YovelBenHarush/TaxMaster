a8e7cfc036d116e7b82df2e4f5ec8ca6a907291c
Shimon Arzuan <58290359+shimonar1171@users.noreply.github.com>
2024-09-18 17:13:23 +0300

    Add AnnualReportIncomeDetailsView (#50)

--- TaxMaster.UI/ViewModels/AnnualReport/AnnualReportIncomeDetailsViewModel.cs
+++ TaxMaster.UI/ViewModels/AnnualReport/AnnualReportIncomeDetailsViewModel.cs
@@ -1,11 +1,46 @@
-﻿namespace TaxMaster
+﻿using TaxMaster.Infra.Entities;
+using TaxMaster.Infra;
+using System.Collections.ObjectModel;
+
+namespace TaxMaster
 {
     public class AnnualReportIncomeDetailsViewModel : BaseViewModel
     {
+        public ObservableCollection<Pair> RegisteredPartner106 { get; set; }
+        public ObservableCollection<Pair> Partner106 { get; set; }
+
+        public bool IsMarried { get; set; }
+
+
+        public AnnualReportIncomeDetailsViewModel()
+        {
+            IsMarried = ReportSettings.Configuration.FamilyStatus == FamilyStatus.Married;
+
+            RegisteredPartner106 = new ObservableCollection<Pair>
+            {
+                new Pair { Key = "Value1A", Value = "Value2A" },
+                new Pair { Key = "Value1B", Value = "Value2B" },
+                new Pair { Key = "Value1C", Value = "Value2C" }
+            };
+
+            Partner106 = new ObservableCollection<Pair>
+            {
+                new Pair { Key = "Value1A", Value = "Value2A" },
+                new Pair { Key = "Value1B", Value = "Value2B" },
+                new Pair { Key = "Value1C", Value = "Value2C" }
+            };
+        }
+
         public async override void OnNext()
         {
             base.OnNext();
             await Shell.Current.GoToAsync(nameof(AnnualReportAbroadAnnexView));
         }
+    }
+
+    public class Pair
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
     }
 }
