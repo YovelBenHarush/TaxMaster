8adc849842102c19dceefc7623dc5381cfcaade5
Shimon Arzuan <58290359+shimonar1171@users.noreply.github.com>
2024-09-19 14:36:14 +0300

    Add General Details View (#63)

---
+++ TaxMaster.UI/Converters/BooleanToYesNoConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace TaxMaster.UI
+{
+    public class BooleanToYesNoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                if (boolValue)
+                {
+                    return "כן";
+                }
+                else
+                {
+                    return "לא";
+                }
+            }
+
+            return "לא";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
