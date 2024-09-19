6a90af0440424536ab35333bb9c53a99979cae12
Shimon Arzuan <sharzuan@microsoft.com>
2024-09-19 11:01:02 +0300

    Add start report

--- TaxMaster.UI/ViewModels/AnnualReport/AnnualReportPersonalDetailsViewModel.cs
+++ TaxMaster.UI/ViewModels/AnnualReport/AnnualReportPersonalDetailsViewModel.cs
@@ -2,6 +2,27 @@
 {
     public class AnnualReportPersonalDetailsViewModel : BaseViewModel
     {
+        private string _guide;
+        public string Guide
+        {
+            get => _guide;
+            set
+            {
+                if (_guide != value)
+                {
+                    _guide = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public AnnualReportPersonalDetailsViewModel()
+        {
+            Guide = GuidesConfigurations.AnnualReportPersonalDetailsGuidePdfUrl;
+        }
+
+
         public async override void OnNext()
         {
             base.OnNext();
