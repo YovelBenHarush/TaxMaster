e9d737488a9624d2649b4e83b702cfb829df71cd
Shimon Arzuan <58290359+shimonar1171@users.noreply.github.com>
2024-09-21 17:30:40 +0300

    Update upload file step (#69)

--- TaxMaster.UI/ViewModels/AnnualReport/AnnualReportAdditionalDocsUploadViewModel.cs
+++ TaxMaster.UI/ViewModels/AnnualReport/AnnualReportAdditionalDocsUploadViewModel.cs
@@ -1,4 +1,5 @@
-﻿using TaxMaster.BL;
+﻿using System.Windows.Input;
+using TaxMaster.BL;
 using TaxMaster.Infra;
 
 namespace TaxMaster
@@ -6,10 +7,24 @@ namespace TaxMaster
     public class AnnualReportAdditionalDocsUploadViewModel : BaseViewModel
     {
         public string OutputPath { get; set; } = ReportSettings.GetOutputDir();
-
+        public ICommand OpenFileExplorerCommand { get; }
         public AnnualReportAdditionalDocsUploadViewModel()
         {
             Title = "העלאת מסמכים נוספים";
+            OpenFileExplorerCommand = new Command(OpenFileExplorer);
+        }
+
+        private void OpenFileExplorer()
+        {
+#if WINDOWS
+            // Only works on Windows to open the folder path in File Explorer
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = OutputPath,
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(psi);
+#endif
         }
 
         public async override void OnNext()
